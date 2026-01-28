80d0a5dd21e1c322e15b517beb036be93de975eb
Jawad Al Bdiwi <github.2snsr@passmail.com>
2026-01-28 19:47:56 +0100

    Final commit

---
+++ task-3/date.js
@@ -0,0 +1,23 @@
+export function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+export function convertMinutesToHours(minutes) {
+  return minutes / 60;
+}
+
+export function convertDaysToHours(days) {
+  return days * 24;
+}
+
+export function convertHoursToDays(hours) {
+  return hours / 24;
+}
+
+export function convertMinutesToSeconds(minutes) {
+  return minutes * 60;
+}
+
+export function convertSecondsToMinutes(seconds) {
+  return seconds / 60;
+}
