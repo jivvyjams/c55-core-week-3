26c348d4be5ceb01e963aa21ba41f6184dfb2bd4
Jawad Al Bdiwi <github.2snsr@passmail.com>
2026-01-28 19:41:51 +0100

    Final commit

--- task-1/book.js
+++ task-1/book.js
@@ -1,3 +1,16 @@
-function isBookApplicable(searchString) {
-  // Your code here
+import PromptSync from "prompt-sync";
+const prompt = PromptSync();
+
+const queryUser = prompt(`Please enter your search query: `);
+const searchString = queryUser.toLowerCase().trim();
+const bookTitle = `The fundamentals of JavaScript`.toLowerCase();
+
+function isBookApplicable() {
+    if (searchString === bookTitle || bookTitle.includes(searchString)) {
+        return true;
+    } else {
+        return false;
 }
+}
+
+console.log(isBookApplicable());
