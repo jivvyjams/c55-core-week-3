6971caa9b4061a15663b408627fdcd05024880ae
Jawad Al Bdiwi <github.2snsr@passmail.com>
2026-01-28 19:42:39 +0100

    Final commit

--- task-2/parse-date.js
+++ task-2/parse-date.js
@@ -1,1 +1,18 @@
-// Your code here
+function parseDateString(dateString) {
+    let parts = dateString.split(" ");
+    let format = parts[0];
+    let date = parts[1];
+    let splitDate = date.split("-");
+
+    let firstDate = Number(splitDate[0]);
+    let secondDate = Number(splitDate[1]);
+    let thirdDate = Number(splitDate[2]);
+
+    if (format === "MDY") {
+        return {day: secondDate, month: firstDate, year: thirdDate}
+    } else if (format === "DMY") {
+        return {day: secondDate, month: firstDate, year: thirdDate}
+    } else {
+        return `Error! Please enter a valid date format.`;
+    }
+}
